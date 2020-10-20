a2399db913af851607b4af58922a9d65c8b0d421
Anoymous <>
2020-10-20 10:44:05 +0900

    setup feature to fetch data from direbase and render it

--- src/tests/actions/expense.test.js
+++ src/tests/actions/expense.test.js
@@ -1,10 +1,18 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+    const expensesData = {};
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt }
+    });
+    database.ref('expenses').set(expensesData).then(() => done());
+});
 
 
 test('should set up remove expense action object', () => {
@@ -83,22 +91,23 @@ test('should add expense to database and store with default value', (done) => {
     });
 });
 
-test('should add expense with default to database and store', () => {
 
+test('should setup set expense action object with data', () => {
+    const action = setExpenses(expenses);
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
 });
 
-
-
-// test('should set up add expense ation object with default values', () => {
-//     const action = addExpense();
-//     expect(action).toEqual({
-//         type: 'ADD_EXPENSE',
-//         expense: {
-//             description: '',
-//             amount: 0,
-//             note: '',
-//             createdAt: 0,
-//             id: expect.any(String)
-//         }
-//     });
-// });
+test('should fetch the expenses from database', (done) => {
+    const store = createMockStore({});
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'SET_EXPENSES',
+            expenses
+        });
+        done();
+    });
+});
